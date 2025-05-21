b80e6577ee210010cc5cbd2232ed7bef65b6d226
Evan Battaglia <ebattaglia@instructure.com>
2025-05-21 21:15:08 +0000

    de-template

---
+++ tests/sendtoterm/sendtoterm_spec.lua
@@ -0,0 +1,12 @@
+local sendtoterm = require("sendtoterm")
+
+describe("setup", function()
+  it("works with default", function()
+    assert(sendtoterm.hello() == "Hello!", "my first function with param = Hello!")
+  end)
+
+  it("works with custom var", function()
+    sendtoterm.setup({ opt = "custom" })
+    assert(sendtoterm.hello() == "custom", "my first function with param = custom")
+  end)
+end)
